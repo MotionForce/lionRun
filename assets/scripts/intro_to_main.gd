a0cadb95e67afd3328e7ab18502710a2b686e2cb
MotionForce <74267742+MotionForce@users.noreply.github.com>
2025-02-19 19:25:40 -0500

    Intro, hotkey changes, font, and merge

---
+++ assets/scripts/intro_to_main.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var game_scene = preload("res://Game_Scene.tscn").instantiate()
+
+func _input(event):
+	if event.is_action_pressed("Skip intro"):
+		get_tree().root.add_child(game_scene)
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "intro_sequence":
+		print("Animation finished")
+		get_tree().change_scene_to_file("res://Game_Scene.tscn")
