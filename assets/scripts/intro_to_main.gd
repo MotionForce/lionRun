f26815aa301ea2918b97e9e86ca5303e77148c40
benb2n <84045122+ben2bn@users.noreply.github.com>
2025-02-20 20:22:02 -0500

    fixed unhandled_input

--- assets/scripts/intro_to_main.gd
+++ assets/scripts/intro_to_main.gd
@@ -3,9 +3,9 @@ extends Control
 var game_scene = preload("res://Game_Scene.tscn").instantiate()
 
 func _unhandled_input(event):
-    if event is InputEventKey:
-        if event.pressed:
-					get_tree().root.add_child(game_scene)
+	if event is InputEventKey:
+		if event.pressed:
+			get_tree().change_scene_to_file("res://Game_Scene.tscn")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "intro_sequence":
