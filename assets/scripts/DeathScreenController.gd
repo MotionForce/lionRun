5a3014221d270b7439492c93e9b058fbec19bbe6
benb2n <84045122+ben2bn@users.noreply.github.com>
2025-02-23 21:43:31 -0500

    main menu sounds

--- assets/scripts/DeathScreenController.gd
+++ assets/scripts/DeathScreenController.gd
@@ -2,17 +2,17 @@ extends Control
 
 @onready var on_click_sound = $OnClickSound
 
-
 func _on_restart_button_down():
 	play_on_click_sound()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 func _on_main_menu_button_down():
 	play_on_click_sound()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://assets/Main_Menu.tscn")
-
 
 func play_on_click_sound():
 	on_click_sound.play()
--- assets/scripts/main_to_game.gd
+++ assets/scripts/main_to_game.gd
@@ -1,8 +1,13 @@
 extends Control
 
 var game_scene = preload("res://assets/Game_Scene.tscn").instantiate()
-
-
+@onready var on_click_play = $OnClickPlay
+@onready var main_menu_theme = $MainMenuTheme
 
 func _on_button_pressed():
+	on_click_play.play()
+	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://assets/Game_Scene.tscn")
+
+func loop_main_menu_theme():
+	main_menu_theme.play()
